36d0ee1aec09e93e42bd93e5fe2bde47f62afe6b
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-08-01 19:52:15 +0600

    Установил express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+app.get('/', (req, res) => {
+
+  res.send('Home');
+
+});
+app.listen(port, ()=>{
+    console.log(`server ${port}`);
+})
+
+
+
