45f5518a2518d50261b6f8f2023333bb93012fea
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-08-02 16:18:50 +0600

    Установил библиотеку caesar-salad, сделал сервер с расшифровкой и шифровкой текста!

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+const { log } = require('console');
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
 
 const port = 8000;
 app.get('/', (req, res) => {
-
-  res.send('Home');
-
+    return res.send('Home');
 });
 app.get('/:word', (req, res) => {
-
-    res.send(req.params.word);
-  
+    return res.send(req.params.word);
 });
 
-
-
+app.get('/cipher/:name', (req, res) => {
+    return res.send(Vigenere.Cipher('password').crypt(req.params.name));
+});
+app.get('/decipher/:name', (req, res) => {
+    return res.send(Vigenere.Decipher('password').crypt(req.params.name));
+});
+app.listen(port, ()=>{
+    console.log(`server ${port}`);
+})
 
